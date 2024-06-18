b97d54d956595724752cbb31f1c01fb8044d3bdf
bharti22v <bharti22v@gmail.com>
2024-06-18 22:43:56 +0100

    My homework week 14

---
+++ src/test/java/resources/testdata/TestData.java
@@ -0,0 +1,33 @@
+package resources.testdata;
+
+import org.testng.annotations.DataProvider;
+
+/**
+ * Created by Jay
+ */
+public class TestData {
+
+
+    @DataProvider(name = "credentials")
+    public Object[][] getData() {
+        Object[][] data = new Object[][]{
+                {"", "","Required"},
+                {"test123@gmail.com", "","Required"},
+                {"", "test123","Required"},
+                {"test123@gmail.com", "test123","Invalid credentials"}
+        };
+        return data;
+    }
+
+    @DataProvider(name = "usertests")
+    public Object[][] getData1() {
+        Object[][] data = new Object[][]{
+                {"Admin1","Admin","Paul Collings","Enable"},
+                {"Cassidy.Hope", "ESS", "Cassidy Hope", "Enable"},
+                {"Nina.Patel", "ESS", "Nina Patel", "Enable"},
+                {"Odis.Adalwin","Admin","Odis Adalwin","Enable"}
+        };
+        return data;
+    }
+
+}
